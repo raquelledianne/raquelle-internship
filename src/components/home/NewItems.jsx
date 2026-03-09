699b03275f5938edd690da31ff5fcedcd082981b
raquelledianne <raquelledschoolcraft@gmail.com>
2026-03-09 08:59:23 -0700

    new-items-fix

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const API_URL =
   "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems";
@@ -16,27 +19,34 @@ const NewItems = () => {
       .catch((err) => console.error("API Error:", err));
   }, []);
 
+  const options = {
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 4 },
+    },
+  };
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
-        <div className="row">
 
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>New Items</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
-          </div>
+        <div className="text-center">
+          <h2>New Items</h2>
+          <div className="small-border bg-color-2"></div>
+        </div>
 
+        <OwlCarousel className="owl-theme" {...options}>
           {items.map((item) => (
-            <div
-              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              key={item.id}
-            >
+            <div className="item" key={item.id}>
               <div className="nft__item">
 
                 <div className="author_list_pp">
-                  <Link to="/author">
+                  <Link to={`/author/${item.authorId}`}>
                     <img
                       className="lazy"
                       src={item.authorImage}
@@ -74,8 +84,8 @@ const NewItems = () => {
               </div>
             </div>
           ))}
+        </OwlCarousel>
 
-        </div>
       </div>
     </section>
   );
